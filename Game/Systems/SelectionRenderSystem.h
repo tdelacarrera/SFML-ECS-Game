0786316b3aae597fbabc9386af6e7e003ba6749c
tdelacarrera <202549429+tdelacarrera@users.noreply.github.com>
2026-03-23 16:31:54 -0300

    Add selection brackets when selecting entities

--- Game/Systems/SelectionRenderSystem.h
+++ Game/Systems/SelectionRenderSystem.h
@@ -4,6 +4,46 @@
 #include <SFML/Graphics.hpp>
 #include "../../Engine/Input/MouseManager.h"
 
+inline void drawSelectionBracket(sf::RenderWindow& window, sf::Vector2f center)
+{
+    float size = 64.f; 
+    float corner = 8.f; 
+    float thickness = 2.f;
+
+    sf::Vector2f half(size / 2.f, size / 2.f);
+
+    sf::Vector2f topLeft = center - half;
+    sf::Vector2f topRight = {center.x + half.x, center.y - half.y};
+    sf::Vector2f bottomLeft = {center.x - half.x, center.y + half.y};
+    sf::Vector2f bottomRight = center + half;
+
+    sf::Color color = sf::Color::Yellow;
+
+    auto makeLine = [&](sf::Vector2f pos, sf::Vector2f size)
+    {
+        sf::RectangleShape line(size);
+        line.setPosition(pos);
+        line.setFillColor(color);
+        window.draw(line);
+    };
+
+    // TOP LEFT
+    makeLine(topLeft, {corner, thickness});
+    makeLine(topLeft, {thickness, corner});
+
+    // TOP RIGHT
+    makeLine({topRight.x - corner, topRight.y}, {corner, thickness});
+    makeLine({topRight.x - thickness, topRight.y}, {thickness, corner});
+
+    // BOTTOM LEFT
+    makeLine({bottomLeft.x, bottomLeft.y - thickness}, {corner, thickness});
+    makeLine({bottomLeft.x, bottomLeft.y - corner}, {thickness, corner});
+
+    // BOTTOM RIGHT
+    makeLine({bottomRight.x - corner, bottomRight.y - thickness}, {corner, thickness});
+    makeLine({bottomRight.x - thickness, bottomRight.y - corner}, {thickness, corner});
+}
+
 inline void SelectionRenderSystem(entt::registry& registry)
 {
     auto& window = registry.ctx().get<WindowResource>().window;
@@ -13,11 +53,7 @@ inline void SelectionRenderSystem(entt::registry& registry)
     {
         auto& transform = view.get<TransformComponent>(entity);
 
-        sf::CircleShape marker(5.f);
-        marker.setFillColor(sf::Color::Yellow);
-        marker.setPosition(transform.position);
-
-        window.draw(marker);
+        drawSelectionBracket(window, transform.position);
     }
 
     auto& mouse = registry.ctx().get<MouseManager>();
@@ -44,6 +80,7 @@ inline void SelectionRenderSystem(entt::registry& registry)
     rect.setPosition(position);
     rect.setSize(size);
 
+    
     rect.setFillColor(sf::Color(0,255,0,40));
     rect.setOutlineColor(sf::Color::Green);
     rect.setOutlineThickness(2);
