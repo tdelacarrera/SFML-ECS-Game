ff8cad05decb5696fa2fa5e6aba3a3bf2f0953e8
tdelacarrera <202549429+tdelacarrera@users.noreply.github.com>
2026-04-10 07:45:33 -0400

    Move resources_ and currentMusicId_ from public to private in MusicManager

--- Engine/Audio/MusicManager.h
+++ Engine/Audio/MusicManager.h
@@ -8,20 +8,22 @@ class MusicManager
 {
 public:
     MusicManager() = default;
-    
-    void load(const std::string& id, const std::string& path);
-    sf::Music& get(const std::string& id) const;
-    void play(const std::string& id, bool loop = true, float volume = 50.f);
+
+    void load(const std::string &id, const std::string &path);
+    sf::Music &get(const std::string &id) const;
+    void play(const std::string &id, bool loop = true, float volume = 50.f);
     void pause();
     void resume();
     void stop();
     void setVolume(float volume);
+
+private:
     std::unordered_map<std::string, std::unique_ptr<sf::Music>> resources_;
     std::string currentMusicId_;
-private:
-    MusicManager(const MusicManager&) = delete;
-    MusicManager& operator=(const MusicManager&) = delete;
 
-    MusicManager(MusicManager&&) = default;
-    MusicManager& operator=(MusicManager&&) = default;
+    MusicManager(const MusicManager &) = delete;
+    MusicManager &operator=(const MusicManager &) = delete;
+
+    MusicManager(MusicManager &&) = default;
+    MusicManager &operator=(MusicManager &&) = default;
 };
